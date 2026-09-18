d90854d954a669eec1b6b1c3773c5481840afd0c
MFA21SCM03D <mdeshmukh1@hawk.iit.edu>
2026-09-19 00:22:53 +0530

    Removed unused import and corrected main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 import { SidePanel } from "./components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+	throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+	<StrictMode>
 		<App />
-	</React.StrictMode>,
+	</StrictMode>,
 );
